fe1affbf9912af2684cbd36f90a4f702e5d65d62
NKSL2001 <s2116593@siswa.um.edu.my>
2024-09-29 15:18:39 +0800

    booking history table feature basic

--- umpoint/umpoint-frontend/src/views/user/BookingHistory.vue
+++ umpoint/umpoint-frontend/src/views/user/BookingHistory.vue
@@ -14,9 +14,9 @@
             <el-tab-pane label="Cancelled" name="cancelled"> </el-tab-pane>
         </el-tabs>
 
-        <el-table>
+        <el-table :data="displayBookings" style="width: 100%; height: 100%">
             <el-table-column type="index" label="#" />
-            <el-table-column prop="invoiceno" label="Invoice No." width="120" />
+            <el-table-column prop="invoiceno" label="Invoice No." width="160" />
             <el-table-column prop="name" label="Facilty/Event" />
 
             <template
@@ -38,33 +38,74 @@
                 <el-table-column
                     prop="bookingDate"
                     label="Booking Date"
-                    width="150"
+                    width="120"
                 />
                 <el-table-column
                     prop="eventDate"
                     label="Event Date"
-                    width="150"
+                    width="120"
                 />
                 <el-table-column
                     prop="action"
+                    fixed="right"
                     label="Action"
-                    width="100"
-                />
+                    width="140"
+                >
+                    <template #default="scope">
+                        <el-button
+                            v-if="activeName == 'pending'"
+                            link
+                            title="Pay for booking"
+                            type="primary"
+                            size="small"
+                            @click.prevent="payForBooking(scope)"
+                        >
+                            <SvgIcon type="mdi" :path="mdiCurrencyUsd" />
+                        </el-button>
+                        <el-button
+                            link
+                            title="Start chat"
+                            type="primary"
+                            size="small"
+                            @click.prevent="startChat(scope)"
+                        >
+                            <SvgIcon type="mdi" :path="mdiForum" />
+                        </el-button>
+                        <el-button
+                            link
+                            title="Cancel booking"
+                            type="danger"
+                            size="small"
+                            @click.prevent="cancelBooking(scope)"
+                        >
+                            <SvgIcon type="mdi" :path="mdiCancel" />
+                        </el-button> </template
+                ></el-table-column>
             </template>
 
-            <el-table-column
-                v-if="activeName == 'all'"
-                prop="status"
-                label="Status"
-                width="100"
-            />
+            <template v-if="activeName == 'all'">
+                <el-table-column
+                    prop="eventDate"
+                    label="Event Date"
+                    width="120"
+                />
+                <el-table-column
+                    prop="status"
+                    fixed="right"
+                    label="Status"
+                    width="100"
+                    :formatter="statusFormatter"
+                />
+            </template>
         </el-table>
     </BaseLayout>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { mdiCurrencyUsd, mdiForum, mdiCancel } from "@mdi/js";
+import { ref, onMounted, onActivated, computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
+import { getCurrentUserBookings } from "@/helpers/api.js";
 
 const router = useRouter();
 const route = useRoute();
@@ -74,4 +115,81 @@ const activeName = ref("pending");
 const handleChangeTab = (tab) => {
     router.push({ path: route.path, query: { view: tab.paneName } });
 };
+
+const initialize = () => {
+    if (route.query.view) {
+        activeName.value = route.query.view;
+    }
+    getBookings();
+};
+
+// get all bookings for this user
+const bookings = ref([]);
+const getBookings = async () => {
+    const response = await getCurrentUserBookings();
+    bookings.value = response.data;
+
+    bookings.value = [
+        {
+            id: 1,
+            invoiceno: "20240101sfafds",
+            name: "Facility 1",
+            status: "pending",
+            bookingDate: "2024-01-01",
+            eventDate: "2024-01-01",
+        },
+    ];
+};
+const displayBookings = computed(() => {
+    if (activeName.value == "all") {
+        return bookings.value;
+    }
+
+    let displayedBookings = bookings.value.filter(
+        (booking) => booking.status.toLowerCase() == activeName.value
+    );
+
+    if (activeName.value == "pending" || activeName.value == "confirmed") {
+        displayedBookings = displayedBookings.map((booking) => {
+            booking.action = "View";
+            return booking;
+        });
+    }
+
+    return displayedBookings;
+});
+const statusFormatter = (row, column) => {
+    return row.status[0].toUpperCase() + row.status.slice(1);
+};
+
+// *** pending actions ***
+const payForBooking = (row) => {
+    console.log("Pay for booking", row);
+};
+const startChat = (row) => {
+    console.log("Start chat", row);
+};
+const cancelBooking = (row) => {
+    console.log("Cancel booking", row);
+};
+
+// refresh
+onMounted(() => {
+    initialize();
+});
+onActivated(() => {
+    initialize();
+});
 </script>
+
+<style scoped>
+.el-button {
+    + .el-button {
+        margin-left: 6px;
+    }
+    svg {
+        width: 1.8em;
+        height: 1.8em;
+    }
+}
+</style>
