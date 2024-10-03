fbeaaff14346f25eed1ac072a43582f479351236
NKSL2001 <s2116593@siswa.um.edu.my>
2024-10-03 23:48:34 +0800

    placeholder home page

--- umpoint/umpoint-frontend/src/views/MainPage.vue
+++ umpoint/umpoint-frontend/src/views/MainPage.vue
@@ -5,8 +5,32 @@
 
         <div id="index-wrap" class="container">
             <div id="index-content">
-                <p>It is a project for the course WAD2.</p>
+                <p>Welcome to UMPoint</p>
             </div>
         </div>
+
+        <el-card v-for="facility in facilities" :key="facility.id">
+            <div slot="header" class="clearfix">
+                <span>{{ facility.name }}</span>
+            </div>
+            <div>
+                <p>{{ facility.description }}</p>
+            </div>
+            <RouterLink :to="`/facility/${facility.id}`">Visit location</RouterLink>
+        </el-card>
     </BaseLayout>
 </template>
+
+<script setup>
+import { getFacilities } from "@/helpers/api";
+import { ref } from "vue";
+
+const facilities = ref([]);
+getFacilities("space").then((response) => {
+    if (response.status !== 200 || response.data.code !== 0) {
+        console.error("Failed to get facilities");
+        return;
+    }
+    facilities.value = response.data.data.list;
+});
+</script>
