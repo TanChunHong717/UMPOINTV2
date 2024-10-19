b6f87af0379919924331bfeca493cad9712d0d64
NKSL2001 <s2116593@siswa.um.edu.my>
2024-10-20 00:06:57 +0800

    spacing

--- umpoint/umpoint-frontend/src/utils/date.ts
+++ umpoint/umpoint-frontend/src/utils/date.ts
@@ -15,6 +15,7 @@ export const addDays = (date: Date, days: number): Date => {
     result.setDate(result.getDate() + days);
     return result;
 }
+
 export const diffDays = (date1: Date, date2: Date): number => {
     const diffTime = Math.abs(date2.getTime() - date1.getTime());
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
