a385cf4efc5e23d9085238c875f2944ad65239c5
NKSL2001 <s2116593@siswa.um.edu.my>
2024-11-07 12:35:00 +0800

    validation rule update

--- umpoint/umpoint-frontend/src/utils/date.ts
+++ umpoint/umpoint-frontend/src/utils/date.ts
@@ -28,3 +28,30 @@ export const sameDay = (date1: Date, date2: Date): boolean => {
         date1.getDate() === date2.getDate()
     );
 }
+
+export const diffHours = (time1: string, time2: String): number => {
+    let date1 = new Date(`2000-01-01T${time1}`);
+    let date2 = new Date(`2000-01-01T${time2}`);
+    const diffTime = Math.abs(date2.getTime() - date1.getTime());
+    return diffTime / (1000 * 60 * 60);
+}
+
+export const sameTime = (time1: string, time2: string): boolean => {
+    let date1 = new Date(`2000-01-01T${time1}`);
+    let date2 = new Date(`2000-01-01T${time2}`);
+    return date1 === date2;
+}
+
+export const listTimesBetween = (startTime: string, endTime: string, gapMinutes: number): string[] => {
+    const times = [];
+    let currentTime = new Date(`2000-01-01T${startTime}`);
+    const endTimeDate = new Date(`2000-01-01T${endTime}`);
+    const gapMilliseconds = gapMinutes * 60 * 1000;
+
+    while (currentTime <= endTimeDate) {
+        times.push(currentTime.toTimeString().slice(0, 5));
+        currentTime = new Date(currentTime.getTime() + gapMilliseconds);
+    }
+
+    return times;
+}
